7d8faa0101ca2585c1caca365a8ce548b16204cd
Karlis Paulus <pauluskarlis@gmail.com>
2024-12-30 18:36:36 +0200

    initial commit

--- person.js
+++ person.js
@@ -1,6 +1,7 @@
 function createPerson (firstName, lastName, age) {
-    return "firstName: " + "'" + firstName + "'," + " lastName: " + "'" + lastName + "'," + " age: " + age ;
+return {
+    firstName: firstName, 
+    lastName: lastName, 
+    age: age
 }
-
-/*const person = createPerson('Max', 'Cool', 35)
-console.log(person) */
+}
