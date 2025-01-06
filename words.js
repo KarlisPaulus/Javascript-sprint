204bc8211fc2db62e3f9ae64a67a20b4107a4f4b
Karlis Paulus <pauluskarlis@gmail.com>
2025-01-06 18:02:43 +0200

    initial commit

---
+++ words.js
@@ -0,0 +1,11 @@
+function splitByDelimiter (input, delimiter) {
+    return input.split(delimiter);
+}
+
+function joinWithDelimiter (input, join) {
+    return input.join(join);
+}
+
+
+//console.log(splitByDelimiter('One, Two, Three', ', '))
+//console.log(joinWithDelimiter(['Four', 'Five', 'Six'], '-*-'))
