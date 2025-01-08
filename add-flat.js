fbe7b63c4c11533eedadf360b81b139fe558bbf4
Karlis Paulus <pauluskarlis@gmail.com>
2025-01-08 19:26:15 +0200

    initial commit

---
+++ add-flat.js
@@ -0,0 +1,9 @@
+function sumNestedArray (input){
+    const flat = input.flat(Infinity);
+    let result = 0;
+    for(const i of flat) {
+        result += i;
+    }
+    return result;
+}
+//console.log(sumNestedArray([1, 2, [3, 'cat', 4, [5, 6]]]))
