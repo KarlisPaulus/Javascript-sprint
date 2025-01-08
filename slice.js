3f81361106addc44215b61c0fbd20ede5764bdb3
Karlis Paulus <pauluskarlis@gmail.com>
2025-01-08 19:06:25 +0200

    typo

--- slice.js
+++ slice.js
@@ -1,7 +1,7 @@
-function sliceFunction(arr, start, end) {
+function sliceFunc(arr, start, end) {
     const startIndex = arr.indexOf(start);
     const endIndex = arr.lastIndexOf(end);
     return arr.slice(startIndex, endIndex + 1);
 }
 //const numbers = ['ant', 'bison', 'camel', 'duck', 'elephant'];
-//console.log(sliceFunction(numbers, 'bison', 'duck'))
+//console.log(sliceFunc(numbers, 'bison', 'duck'))
