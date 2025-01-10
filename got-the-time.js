be6f615ca95bb35f834c48edcb6afbc01f8095fa
Karlis Paulus <pauluskarlis@gmail.com>
2025-01-10 11:37:09 +0200

    result between backties

--- got-the-time.js
+++ got-the-time.js
@@ -20,8 +20,7 @@ function printPrettyDate (input) {
         hours = 12;
     }
     const paddedHours = hours.toString().toString().padStart(2, '0')
-    const result = ("Today is " + day + ", " + month + " " + date + ", " + year + ", " + "and the time is "
-         + paddedHours + ":" + minutes + ":" + seconds + " " + amOrPm);
+    const result = `Today is ${day}, ${month} ${date}, ${year}, and the time is ${paddedHours}:${minutes}:${seconds} ${amOrPm}`;
     return result;
 }
 //console.log(printPrettyDate("1940-06-06T05:06:13.723Z"))
