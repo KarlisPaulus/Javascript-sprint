be8a8871c801b8743e332bc7b52982a6a7eb2136
Karlis Paulus <pauluskarlis@gmail.com>
2025-01-15 12:20:30 +0200

    initial commit

---
+++ authorized-users.js
@@ -0,0 +1,17 @@
+function isAuthorizedUser (input) {
+    return function(id) {
+        return input.includes(id);
+    }
+}
+/*
+const check = isAuthorizedUser([1, 2, 3])
+
+console.log(check(5))
+console.log(check(4))
+console.log(check(3))
+console.log(check(2))
+console.log(check(1))
+
+console.log(isAuthorizedUser([4, 5, 6])(10))
+console.log(isAuthorizedUser(['A', 'B', 'C'])('C'))
+*/
