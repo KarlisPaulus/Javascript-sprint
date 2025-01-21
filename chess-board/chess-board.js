23eb52572d9b73f5d1480d77a9fac877fee5c185
Karlis Paulus <pauluskarlis@gmail.com>
2025-01-21 16:15:26 +0200

    initial commit

---
+++ chess-board/chess-board.js
@@ -0,0 +1,30 @@
+function initializeChessboard() {
+    const chessboard = document.querySelector(".chessboard");
+    let previousSquare = null;
+
+    for (let row = 1; row <= 8; row++) {
+        for (let column = 1; column <= 8; column++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.id = `square-${row}-${column}`;
+
+            if ((row + column) % 2 === 0) {
+                square.classList.add("white")
+            } else {
+                square.classList.add("black");
+            }
+
+            square.addEventListener("click", () => {
+                if (previousSquare) {
+                    previousSquare.style.backgroundColor = previousSquare.classList.contains("white") ? "white" : "black";
+                }
+                square.style.backgroundColor = "red";
+                previousSquare = square;
+            });
+
+            chessboard.appendChild(square);
+        }
+    }
+}
+
+initializeChessboard();
